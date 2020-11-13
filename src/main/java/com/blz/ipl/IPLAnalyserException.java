7551d6d481e83a943caadfc1533cb527defb9450
Your Name <you@example.com>
2020-11-13 19:48:04 +0530

    Added: custom exception to handle exceptions

---
+++ src/main/java/com/blz/ipl/IPLAnalyserException.java
@@ -0,0 +1,15 @@
+package com.blz.ipl;
+
+public class IPLAnalyserException extends Exception {
+
+	enum IPLAnalyserExceptionType {
+		CENSUS_FILE_PROBLEM, INCORRECT_TYPE, SOME_OTHER_ERRORS, PARSE_ERROR
+	}
+
+	IPLAnalyserExceptionType exceptionType;
+
+	public IPLAnalyserException(String message, IPLAnalyserExceptionType exceptionType) {
+		super(message);
+		this.exceptionType = exceptionType;
+	}
+}
