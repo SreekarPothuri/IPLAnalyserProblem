f602cafc0513258e693c625bab83315bc608ad0b
Your Name <you@example.com>
2020-11-13 20:46:00 +0530

    Added: Test case to findTopBattingStrikeRate

--- src/test/java/com/blz/ipl/IPLAnalyserTest.java
+++ src/test/java/com/blz/ipl/IPLAnalyserTest.java
@@ -6,7 +6,6 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.blz.ipl.IPLAnalyserException.IPLAnalyserExceptionType;
 import com.google.gson.Gson;
 
 public class IPLAnalyserTest {
@@ -15,38 +14,20 @@ public class IPLAnalyserTest {
 	private static final String WRONG_CENSUS_CSV = "F:\\BridgeLabz Fellowship Program\\practice\\IPLAnalyserProblem\\src\\test\\resources\\MostRun.csv";
 	private static final String WRONG_TYPE_CENSUS_CSV = "F:\\BridgeLabz Fellowship Program\\practice\\IPLAnalyserProblem\\src\\test\\resources\\MostRuns.pdf";
 
-	static IPLAnalyser iplbatting;
+	static IPLAnalyser ipl;
 
 	@BeforeClass
 	public static void obj() {
-		iplbatting = new IPLAnalyser();
+		ipl = new IPLAnalyser();
 	}
 
 	@Test
 	public void getNumOfRecords_ShouldReturnCount() {
 		try {
-			int numOfRecords = iplbatting.loadBatsmenData(RIGHT_CENSUS_CSV);
+			int numOfRecords = ipl.loadBatsmenData(RIGHT_CENSUS_CSV);
 			Assert.assertEquals(100, numOfRecords);
 		} catch (IPLAnalyserException e) {
 			e.printStackTrace();
-		}
-	}
-
-	@Test
-	public void givenWrongCsvFile_ShouldThrowIPLAnalyserExceptionOfTypeCensusFileProblem() {
-		try {
-			new IPLAnalyser().loadBatsmenData(WRONG_CENSUS_CSV);
-		} catch (IPLAnalyserException e) {
-			assertEquals(IPLAnalyserExceptionType.CENSUS_FILE_PROBLEM, e.exceptionType);
-		}
-	}
-
-	@Test
-	public void givenWrongTypeCsvFile_ShouldThrowIPLAnalyserExceptionOfTypeIncorrectType() {
-		try {
-			new IPLAnalyser().loadBatsmenData(WRONG_TYPE_CENSUS_CSV);
-		} catch (IPLAnalyserException e) {
-			assertEquals(IPLAnalyserExceptionType.INCORRECT_TYPE, e.exceptionType);
 		}
 	}
 
@@ -56,9 +37,22 @@ public class IPLAnalyserTest {
 		try {
 			String sortedBatsmenJson = new IPLAnalyser().getSortedBatsmenListOnBattingAverage(RIGHT_CENSUS_CSV);
 			IPLBattingAnalysis[] batsmenListCsv = new Gson().fromJson(sortedBatsmenJson, IPLBattingAnalysis[].class);
-			assertEquals("MS Dhoni", batsmenListCsv[0].playerName);
+			assertEquals(83.2, batsmenListCsv[0].avg, 0.0);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	//UC2
+	@Test
+	public void givenSortedOnBattingStrikeRateBatsmenList_ShouldReturnBestStrikeRatedBatsman() {
+		try {
+			String sortedBatsmenJson = new IPLAnalyser().getSortedBatsmenListOnBattingStrikeRate(RIGHT_CENSUS_CSV);
+			IPLBattingAnalysis[] batsmenListCsv = new Gson().fromJson(sortedBatsmenJson, IPLBattingAnalysis[].class);
+			assertEquals(333.33, batsmenListCsv[0].strikeRate, 0.0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
