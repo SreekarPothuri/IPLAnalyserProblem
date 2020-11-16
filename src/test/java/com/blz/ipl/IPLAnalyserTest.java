c538ebbb12ff7b50e947d015f914e66b4683fabe
Your Name <you@example.com>
2020-11-16 08:52:01 +0530

    Added test case to check top batting strikerate

--- src/test/java/com/blz/ipl/IPLAnalyserTest.java
+++ src/test/java/com/blz/ipl/IPLAnalyserTest.java
@@ -1,7 +1,5 @@
 package com.blz.ipl;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -10,49 +8,42 @@ import com.google.gson.Gson;
 
 public class IPLAnalyserTest {
 
-	private static final String RIGHT_CENSUS_CSV = "F:\\BridgeLabz Fellowship Program\\practice\\IPLAnalyserProblem\\src\\test\\resources\\MostRuns.csv";
-	private static final String WRONG_CENSUS_CSV = "F:\\BridgeLabz Fellowship Program\\practice\\IPLAnalyserProblem\\src\\test\\resources\\MostRun.csv";
-	private static final String WRONG_TYPE_CENSUS_CSV = "F:\\BridgeLabz Fellowship Program\\practice\\IPLAnalyserProblem\\src\\test\\resources\\MostRuns.pdf";
+	private static final String RIGHT_MOST_RUNS_CSV = "F:\\BridgeLabz Fellowship Program\\practice\\IPLAnalyserProblem\\src\\test\\resources\\MostRuns.csv";
+	private static final double DELTA = 1e-15;
 
-	static IPLAnalyser ipl;
+	static IPLAnalyser iplbatting;
 
 	@BeforeClass
 	public static void obj() {
-		ipl = new IPLAnalyser();
+		iplbatting = new IPLAnalyser();
 	}
 
 	@Test
-	public void getNumOfRecords_ShouldReturnCount() {
+	public void givenMostRunsCSV_ShouldReturnNumberOfRecords() throws IPLAnalyserException {
+		Assert.assertEquals(101, iplbatting.loadCSVData(RIGHT_MOST_RUNS_CSV));
+	}
+
+	@Test
+	public void givenMostRunsCSV_ShouldReturnTopBattingAverages() {
 		try {
-			int numOfRecords = ipl.loadBatsmenData(RIGHT_CENSUS_CSV);
-			Assert.assertEquals(100, numOfRecords);
+			iplbatting.loadCSVData(RIGHT_MOST_RUNS_CSV);
+			String sortedData = iplbatting.getTopBattingAverages();
+			IPLBattingAnalysis[] censusCSV = new Gson().fromJson(sortedData, IPLBattingAnalysis[].class);
+			Assert.assertEquals(83.2, censusCSV[censusCSV.length - 1].avg, DELTA);
 		} catch (IPLAnalyserException e) {
 			e.printStackTrace();
 		}
 	}
 
-	//UC1
 	@Test
-	public void givenSortedOnBattingAverageBatsmenList_ShouldReturnBestAveragedBatsman() {
+	public void givenMostRunsCSV_ShouldReturnTopStrikingRatesOfBatsman() {
 		try {
-			String sortedBatsmenJson = new IPLAnalyser().getSortedBatsmenListOnBattingAverage(RIGHT_CENSUS_CSV);
-			IPLBattingAnalysis[] batsmenListCsv = new Gson().fromJson(sortedBatsmenJson, IPLBattingAnalysis[].class);
-			assertEquals(83.2, batsmenListCsv[0].avg, 0.0);
-		} catch (Exception e) {
+			iplbatting.loadCSVData(RIGHT_MOST_RUNS_CSV);
+			String sortedData = iplbatting.getTopStrikingRates();
+			IPLBattingAnalysis[] mostRunsCSV = new Gson().fromJson(sortedData, IPLBattingAnalysis[].class);
+			Assert.assertEquals(333.33, mostRunsCSV[mostRunsCSV.length - 1].avg, DELTA);
+		} catch (IPLAnalyserException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	//UC2
-	@Test
-	public void givenSortedOnBattingStrikeRateBatsmenList_ShouldReturnBestStrikeRatedBatsman() {
-		try {
-			String sortedBatsmenJson = new IPLAnalyser().getSortedBatsmenListOnBattingStrikeRate(RIGHT_CENSUS_CSV);
-			IPLBattingAnalysis[] batsmenListCsv = new Gson().fromJson(sortedBatsmenJson, IPLBattingAnalysis[].class);
-			assertEquals(333.33, batsmenListCsv[0].strikeRate, 0.0);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 }
